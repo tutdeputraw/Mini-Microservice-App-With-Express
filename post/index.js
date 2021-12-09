e4f4eafbbc56791581a35d04757dce02c023b008
tutdeputraw <anoboy.code@gmail.com>
2021-12-09 14:12:33 +0700

    Comments service creation

--- post/index.js
+++ post/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const randomBytes = require('crypto');
+const {
+  randomBytes
+} = require('crypto');
 
 const app = express();
 app.use(bodyParser.json());
